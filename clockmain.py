7cdb04ad8cd156c255feccd51661a62c8fd83923
dillon <dhall@motocol.com>
2017-03-26 17:12:56 -0600

    added more exsamples to time_learning and deleted some useless stuff from main

--- clockmain.py
+++ clockmain.py
@@ -15,20 +15,14 @@ class AlarmClock:
         # see if a file containging the last alarm time exists, if not then get the current time and update it to the new file
         if os.path.isfile(self.filename):
             # file does exist
-            filenameIO=open(self.filename,"r+")
-            buffer=filenameIO.read(30)
-            lastwakeup=json.loads(buffer) # take the json containing the last time and the last ring date and the datetime when it was was accessed
-            if (time.time()-lastwakeup.timestamp) > 100000:
-                newjson=json.dumps({"timestamp":time.time,"wakeuptime":getWakeupEvent()})
-                filenameIO.write(newjson)
+            print("doing stuff for a file")
         else:
-            filenameIO=open(self.filename,"w+")
-            t=time.time()
-            print(t)
-            #newjson=json.dumps({"timestamp":t,"wakeuptime":getWakeupEvent()})
-            #filenameIO.write(newjson)
+            # if the file does not exist, then create one
+            now=datetime.datetime.now()
+            jsonbuffer=json.dumps({"timestamp":str(now),"savetime":str(getWakeupEvent)})
+            print(jsonbuffer)
 
-        filenameIO.close()
+        #filenameIO.close()
 
 
 #from digitobj import Digit # import the digit class
--- time_learning.py
+++ time_learning.py
@@ -3,10 +3,12 @@
 # https://docs.python.org/2/library/time.html#module-time
 # https://docs.python.org/2/library/datetime.html#datetime.datetime
 # https://www.tutorialspoint.com/python/python_date_time.htm
+# http://stackoverflow.com/questions/466345/converting-string-into-datetime
 
 import time
 import datetime
 import json
+import dateutil.parser
 from listevents import getWakeupEvent
 
 
@@ -17,7 +19,13 @@ if __name__ == "__main__":
     #print(getWakeupEvent().time().second)
     #print(getWakeupEvent().time().time)
     now=datetime.datetime.now()
-    a=json.dumps({"timestamp":str(time.time()),"savetime":str(now)})
-    print(a)
-    b=json.loads(a) # returns a dictionary 
-    print(b["savetime"])
+    wakeuptime=getWakeupEvent()
+    #print(wakeuptime.__str__())
+    a=json.dumps({"timestamp":str(now),"savetime":wakeuptime.__str__()})
+    #print(wakeuptime)
+    #print(a)
+    b=json.loads(a) # returns a dictionary
+    print("time stamp ",b["timestamp"])
+    print("save time ",b["savetime"])
+    dt=dateutil.parser.parse(b["savetime"])
+    print(dt.time().hour)
