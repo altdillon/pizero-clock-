f92c423d00bbd060a61e4e9538609032d4acb3f9
dillon <dhall@motocol.com>
2017-03-26 04:24:11 -0600

    played with some exsamples for datetime and saved some links to time_learning.py

--- time_learning.py
+++ time_learning.py
@@ -1,3 +1,18 @@
 # for learning about time formats in python
+# http://www.saltycrane.com/blog/2008/06/how-to-get-current-date-and-time-in/
+# https://docs.python.org/2/library/time.html#module-time
+# https://docs.python.org/2/library/datetime.html#datetime.datetime
+# https://www.tutorialspoint.com/python/python_date_time.htm
+
 import time
+import datetime
 from listevents import getWakeupEvent
+
+
+
+if __name__ == "__main__":
+    print(getWakeupEvent().time().hour)
+    print(getWakeupEvent().time().minute)
+    print(getWakeupEvent().time().second)
+    #print(getWakeupEvent().time().time)
+    now=datetime.now(time.time())
