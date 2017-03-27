93025c4d5eaf94eb521312e81038fc6d9f205c07
dillon <dhall@motocol.com>
2017-03-26 20:29:58 -0600

    added functionality for saving and loading that day's wakeup time

--- clockmain.py
+++ clockmain.py
@@ -1,5 +1,5 @@
 from listevents import getWakeupEvent
-from dateutil import parser
+import dateutil.parser
 import datetime
 import time
 import json
@@ -14,13 +14,34 @@ class AlarmClock:
     def update_time_file(self):
         # see if a file containging the last alarm time exists, if not then get the current time and update it to the new file
         if os.path.isfile(self.filename):
-            # file does exist
-            print("doing stuff for a file")
+
+            filenameIO=open(self.filename,"r+")
+            jsonbuffer=filenameIO.read(150)
+            dt=json.loads(jsonbuffer)
+            # figure out when this json file was last updated
+            last_timestamp=dateutil.parser.parse(dt["timestamp"])
+            deltadatetime=datetime.datetime.now()-last_timestamp
+            # if more than 1 hour has ellisped then update the json file, time is in seconds
+            if deltadatetime.seconds > 3600:
+                # close the file and then re open it with diffent permissions
+                filenameIO.close()
+                filenameIO=open(self.filename,"w+")
+                # update the time and write it to the json file
+                wakeupevent=getWakeupEvent() # update the wakeup event from the server
+                newjsondata={"timestamp":str(datetime.datetime.now()),"savetime":wakeupevent.__str__()}
+                filenameIO.write(json.dumps(newjsondata))
+
+            # else, we don't really need to do any thing
+
+            filenameIO.close()
         else:
             # if the file does not exist, then create one
+            filenameIO=open(self.filename,"w+")
             now=datetime.datetime.now()
-            jsonbuffer=json.dumps({"timestamp":str(now),"savetime":str(getWakeupEvent)})
-            print(jsonbuffer)
+            wakeuptime=getWakeupEvent()
+            jsonbuffer=json.dumps({"timestamp":str(now),"savetime":wakeuptime.__str__()})
+            filenameIO.write(jsonbuffer)
+            filenameIO.close()
 
         #filenameIO.close()
 
